f6e465da678c244613d9cb48641f391c73cb6443
bendevera <ben10devera@gmail.com>
2020-03-16 21:24:09 -0600

    updating how config is set

--- project/server/__init__.py
+++ project/server/__init__.py
@@ -1,11 +1,11 @@
 # project/server/__init__.py
-
 import os
-
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from project.server.config import DevelopmentConfig, ProductionConfig
+
 
 app = Flask(__name__)
 CORS(app)
@@ -14,7 +14,7 @@ app_settings = os.getenv(
     'APP_SETTINGS',
     'project.server.config.ProductionConfig'
 )
-app.config.from_object(app_settings)
+app.config.from_object(ProductionConfig)
 app.config['STATIC_FOLDER'] = 'build'
 
 bcrypt = Bcrypt(app)
